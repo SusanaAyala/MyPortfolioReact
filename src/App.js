a0f462a9da9dd2e12c16ed6ff15123295a474c45
SusanaAyala <s.ayala@hotmail.co.uk>
2025-04-08 23:16:41 +0100

    Fix CSS error in base.css and prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Hero from "./components/Hero";
-// import Testimonials from "./components/Testimonials";
 
-// // Import Library CSS
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
 
 
 import "./styles/base.css";
