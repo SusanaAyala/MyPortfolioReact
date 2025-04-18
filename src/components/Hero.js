411c492d699739ba5d81e36147d2f6519a5241d9
SusanaAyala <s.ayala@hotmail.co.uk>
2025-04-18 03:10:12 +0100

    intro

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -165,7 +165,7 @@ function Hero() {
       </section>
       <section className="experience">
         <p>
-          Hello! I’m Susie, a passionate front-end developer specializing in React, CSS, JavaScript, and Shopify Theme Development. I thrive on crafting engaging, responsive, and seamless designs, working closely with clients to bring their visions to life. Check out my work here👇
+          Hello! I’m Susie, a passionate front-end developer specializing in React, WordPress, CSS, JavaScript, and Shopify Theme Development. I thrive on crafting engaging, responsive, and seamless designs, working closely with clients to bring their visions to life. Check out my work here👇
         </p>
       </section>
       <section className="cta">
